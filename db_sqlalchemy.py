e4d3f0fdbfae749d226be7bb41b97a9495a66e72
Arii19 <arianer19@hotmail.com>
2025-11-16 21:44:54 -0300

    removendo git add do lugar erado

--- db_sqlalchemy.py
+++ db_sqlalchemy.py
@@ -7,7 +7,7 @@ from datetime import datetime
 DATABASE_URL = os.getenv("External_Database_URL")  # Ex: 'postgresql://user:password@host:port/dbname'
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
-git add .
+
 class ChatHistory(Base):
     __tablename__ = "chat_history"
     __table_args__ = {"schema": "chatapp"}  # Schema customizado
